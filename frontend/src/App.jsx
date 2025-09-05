c9ba0c5917b05b2b736b7d32ec05d654e31af7e4
arttxyz <albertiniarthur13@gmail.com>
2025-09-05 13:59:31 -0300

    3. Melhorando o App.jsx e adicionando o App.css

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Carros from './pages/Carros';
+import Lojas from './pages/Lojas';
+import Sobre from './pages/Sobre';
+import Contato from './pages/Contato';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/carros" element={<Carros />} />
+            <Route path="/lojas" element={<Lojas />} />
+            <Route path="/sobre" element={<Sobre />} />
+            <Route path="/contato" element={<Contato />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
